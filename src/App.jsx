89691e0fef03c9f139e6699ac312e02d55a441cb
ulkar-akhmedli <ulkar.ahmedli014@gmail.com>
2023-05-16 16:36:34 +0400

    /intall react

---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Card from './components/Card/Card'
+
+const App = () => {
+  return (
+    <div>
+      <Card/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/components/Card/Card.jsx
@@ -0,0 +1,10 @@
+import React from "react";
+// import product from "../../assets/phone.svg";
+
+const Card = () => {
+  return <div className="product">
+    Card
+  </div>;
+};
+
+export default Card;
